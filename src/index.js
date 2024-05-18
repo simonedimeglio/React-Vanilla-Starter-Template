ebf437bb63069388f53bccc7510bc4759d3d93aa
simonedimeglio <simonedimeglio@icloud.com>
2024-05-18 19:56:22 +0200

    init

--- src/index.js
+++ src/index.js
@@ -1,17 +1,27 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+// Importa la libreria React.
+import React from "react";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+// Importa la libreria ReactDOM.
+// ReactDOM è utilizzata per interagire con il DOM del browser,
+// ovvero la struttura ad albero degli elementi HTML.
+import ReactDOM from "react-dom/client";
+
+// Importa il file di stile globale.
+import "./index.css";
+
+// Importa il componente principale della tua applicazione, chiamato App.
+// Questo componente è il cuore della tua applicazione React.
+import App from "./App";
+
+// Crea un punto di ingresso per l'applicazione React nel DOM.
+// Trova l'elemento con l'id "root" nel file index.html e lo usa come contenitore per l'app React.
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+// Utilizza il metodo render per visualizzare il contenuto dell'applicazione React all'interno del contenitore "root".
 root.render(
+  // React.StrictMode è un componente che aiuta a individuare potenziali problemi nel codice. Attiva avvisi aggiuntivi durante lo sviluppo, ma non influisce sulla produzione.
   <React.StrictMode>
+    {/* Renderizza il componente App. Questo è il punto in cui il componente App viene montato e visualizzato nel DOM. */}
     <App />
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
